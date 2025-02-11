dbd6bdffe392cd9510a369bf163cf8cb70ecf8c0
Avvud <avvudaiyappanrm@gmail.com>
2025-02-11 09:10:12 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,8 @@
-print("hello world")
+import streamlit as st
+
+# App Title
+st.title("📝 Simple Text Analyzer")
+
+# Sidebar
+st.sidebar.header("Settings")
+user_text = st.text_area("Enter your text here:"
